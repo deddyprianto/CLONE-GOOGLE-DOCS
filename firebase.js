ae69ae95fbdb61e54855c8d174ee04206a22ad78
deddyprianto <haicoders12@gmail.com>
2021-07-20 20:32:18 +0700

    add a new thing

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDBY7mmmOUvTzN4qtxtJIUzPwdXqUDAB3Y",
+  authDomain: "aplikasi-demo-clone.firebaseapp.com",
+  projectId: "aplikasi-demo-clone",
+  storageBucket: "aplikasi-demo-clone.appspot.com",
+  messagingSenderId: "602746713314",
+  appId: "1:602746713314:web:91dc8ac7bc1af8eeb49abb",
+  measurementId: "G-6ZSBT7FEBT",
+};
---
+++ pages/components/Header.js
@@ -0,0 +1,51 @@
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white">
+      <Button
+        color="gray"
+        buttonType="outline"
+        rounded={true}
+        iconOnly={true}
+        ripple="dark"
+        className="hidden md:inline-flex h-20 w-20 border-0"
+      >
+        <Icon name="menu" size="3xl" />
+      </Button>
+      <Icon name="description" size="5xl" color="blue" />
+      <h1 className="hidden md:inline-flex ml-2 text-gray-700 text-2xl">
+        Docs
+      </h1>
+      {/* input */}
+      <div className="mx-5 md:mx-20 focus-within:text-gray-600 focus-within:shadow-md  flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg">
+        <Icon name="search" size="3xl" color="gray" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="flex-grow px-5 text-base bg-transparent outline-none"
+        />
+      </div>
+      {/* button login */}
+      <Button
+        color="gray"
+        buttonType="outline"
+        rounded={true}
+        iconOnly={true}
+        ripple="dark"
+        className="hidden md:inline-flex ml-5 md:ml-20 h-20 w-20 border-0"
+      >
+        <Icon name="apps" size="3xl" color="gray" />
+      </Button>
+      <img
+        src=""
+        className="cursor-pointer h-12 w-12 rounded-full ml-2 "
+        loading="lazy"
+        alt=""
+      />
+    </header>
+  );
+};
+
+export default Header;
